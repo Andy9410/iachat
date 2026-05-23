303392e744cc25a0b8498612778495cab3f69765
Andy Barreiro <andybarreiro23cb@gmail.com>
2026-05-23 20:25:54 -0300

    stream chat

--- src/main/java/com/academy/chatservice/config/OpenRouterProperties.java
+++ src/main/java/com/academy/chatservice/config/OpenRouterProperties.java
@@ -8,7 +8,7 @@ public record OpenRouterProperties(
         @DefaultValue("https://openrouter.ai") String baseUrl,
         @DefaultValue("liquid/lfm-2.5-1.2b-instruct:free") String model,
         String apiKey,
-        @DefaultValue("true") boolean reasoningEnabled,
+        @DefaultValue("false") boolean reasoningEnabled,
         @DefaultValue("10") int connectTimeoutSeconds,
         @DefaultValue("120") int requestTimeoutSeconds
 ) {}
